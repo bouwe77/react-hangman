78d4333989ee659cfc06fefcf4af8e61c5ab9475
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-02-01 09:43:30 +0100

    .

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,8 +1,28 @@
 import React from "react";
 
+import StartNewGame from "./StartNewGame";
+
 class Hangman extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //this.maxAttempts = 5;
+    //this.words = ["hoi", "hallo", "moio"];
+
+    this.state = {
+      //  currentWord: "",
+      //  attempts: 0,
+      //  correctLetters: [],
+      //  incorrectLetters: []
+    };
+  }
+
   render() {
-    return <div>hiero!</div>;
+    return (
+      <div>
+        <StartNewGame />
+      </div>
+    );
   }
 }
 
