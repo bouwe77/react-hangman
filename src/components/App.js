6607a0263511dbcf632395f2b5c0cc8d221de74b
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-02-01 09:33:31 +0100

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import Header from "./Header";
+import Hangman from "./Hangman";
 
 const App = () => (
   <div className="App">
     <Header />
+    <Hangman />
   </div>
 );
 
---
+++ src/components/Hangman.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Hangman extends React.Component {
+  render() {
+    return <div>hiero!</div>;
+  }
+}
+
+export default Hangman;
